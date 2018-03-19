1e0f125c77faf5f780b7c92364c042e96439cea7
Sean Widmier <seanwidmier@gmail.com>
2018-03-19 17:06:30 -0500

    ContinuousServo library
    ContinuousServo provides all of the functions used for deflecting the
    parafoil to achieve a necessary change in heading degree and direction.

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # RAPTOR
 Ram Air Parafoil Target Object Return (RAPTOR) is a system designed for automated return of high-altitude balloning payloads. 
+
+
+Helpful link for installing libraries: https://www.arduino.cc/en/hacking/libraries
+
+Summary:
+	Copy the contents of the 'libraries' folder into Documents\Arduino\libraries
+	The RAPTOR.ino file should then happily compile in the Arduino IDE.
---
+++ libraries/ContinuousServo/ContinuousServo.cpp
@@ -0,0 +1,79 @@
+/*
+
+*/
+#include "ContinuousServo.h"
+#include "Arduino.h"
+
+/* Public Methods */
+
+/*
+ *	Constructor for ContinuousServo accepts arguments for the pin and the
+ *   time to rotate (ttr) in milliseconds.
+ */
+ContinuousServo::ContinuousServo(int pin){
+  _servo.attach(pin);
+}
+
+/*
+ *	servoAdjustment acts as the wrapper for the rest of the methods,
+ *   accepting inputs of how much you want to turn in degrees (deg) and in what dir (dir).
+ */
+void ContinuousServo::servoAdjustment(float deg, int dir){
+	int duration = timeToTurn(deg);		
+	
+	manualAdjustment(dir);
+	delay(duration);	// run a delay for the required duration in milliseconds
+	//busyDelay(duration); *******TODO
+	// Did it work
+	resetServos(dir);
+}
+
+/* Private Methods */
+
+/*
+ *	timeToTurn calculates the duration of the turn in milliseconds based on the degrees given in servoAdjustment.
+ *	 The function will select a deflection setting for the user, based on if the turn will take more or less than a second to execute.
+ */
+int ContinuousServo::timeToTurn(float degree){
+	if(degree / float(_dPSH) >= 1){	// If the turn takes more than one second, use high deflection
+		_currentdef = HIGH;
+		return degree/_dPSH;
+	}
+	else{					// Otherwise use low deflection
+		_currentdef = LOW;
+		return degree/_dPSL;
+	}
+}
+
+/*
+ *	resetServos will reset the servos to the default position after a turn.
+ *   Must be called before timeToTurn is used, as it relies on the previous turn's deflection setting.
+ */
+void ContinuousServo::resetServos(int dir){
+	if(dir == RIGHT)
+		manualAdjustment(LEFT);
+	else
+		manualAdjustment(RIGHT);
+}
+
+/*
+ *	manualAdjustment adjusts the servos so that they achieve the necessary deflection setting
+ *   for the input direction.
+ */
+void ContinuousServo::manualAdjustment(int dir){
+	_servo.write(SERVO_STOP);				// Stop the servos just in-case they're running already.
+	if(_currentdef == HIGH){				// Check deflection setting set by timeToTurn
+		if(dir == RIGHT)					
+			_servo.write(_hspeed);			// If it is a right turn, just use the deflection setting speed.
+		else
+			_servo.write(_hspeed + 90);		// Otherwise, add 90 to the speed to reverse the direction.
+	}
+	else{									// deflection is LOW
+		if(dir == RIGHT)
+			_servo.write(_lspeed);
+		else
+			_servo.write(_lspeed + 90);
+	}
+	delay(_ttr);				// Delay the set amount of time to get to the deflection setting.
+	_servo.write(SERVO_STOP);				// Stop the servos once we've reached the deflection setting.
+}
