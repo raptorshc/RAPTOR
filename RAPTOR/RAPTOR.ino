fe04578e5f5b4f5fade08820ba2a392a7ea86d61
Sean Widmier <seanwidmier@gmail.com>
2018-04-16 21:09:04 -0500

    debug pressure + temp + alt + sd card code

--- RAPTOR/RAPTOR.ino
+++ RAPTOR/RAPTOR.ino
@@ -1,5 +1,7 @@
-#include <Servo.h>
 #include <SFE_BMP180.h>
+#include <Wire.h>
+#include <Servo.h>
+#include <SD.h>
 
 #define SERVO_PIN 6
 #define SERVO_STOP 90
@@ -8,95 +10,157 @@
 #define SOLENOID_PIN 5
 #define SOLENOID_SWITCH 6
 
-#define TEMP_PIN 4
-
 Servo servo;
 SFE_BMP180 pressure;
+File file;
 
-double baseline;
+float baseline;
 
 void setup() {
+  Serial.begin(9600);
   servo.attach(6);
   pinMode(SERVO_SWITCH, INPUT);
   pinMode(SOLENOID_PIN, OUTPUT);
+
+  /* Pressure */
   pressure.begin();
   baseline = getPressure();
-  Serial.begin(9600);
+  
+  /* SD Card */
+  pinMode(10, OUTPUT);
+  SD.begin(10);
+  file = SD.open("data.txt", FILE_WRITE);
+
+  file.print("Baseline = ");
+  file.println(baseline);
+  
+  file.println("TEMPERATURE | PRESSURE | ALTITUDE | SERVO 1 | SERVO 2 | SOLENOID 1 | SOLENOID 2");
+  file.close();
 }
 
 void loop() {
-  getTemperature();
-  servoTest(1000);
-  solenoidTest(200);
-  P = getPressure();
-  Serial.write("Pressure = %d", P);
-  Serial.write("Altitude = %d", pressure.altitude(P, baseline));
+  file = SD.open("data.txt", FILE_WRITE);
+  double P = getPressure();
+  file.print(P);
+  file.print("        ");
+  file.print(pressure.altitude(baseline, P));
+  file.print("      ");
+  
+//  servoTest(1000);
+//  solenoidTest(200);
+
+  file.println(" ");
+  file.close();
   delay(5000);
 }
 
-/*
- * getTemperature will use the appropriate ADC reading to calculate temperature and print it to the SD card.
- */
-void getTemperature(void){
-  int read_adc = analogRead(TEMP_PIN);                        // save reading
+double getPressure(){
+  char status;
+  double T,P,p0,a;
+
+  // You must first get a temperature measurement to perform a pressure reading.
   
-  float voltage = (0.000502512562*read_adc - 0.095979899);    //converts the adc reading into an actual voltage based off of slope.
-  float resistance = (3.3*10000)/(3.3 - voltage) - 10000;     //Need to convert our voltage reading into the resistance across the thermistor. To find this we use 3.3(R1)/(3.3-vadc) - R1
-  float a_const = 0.003354016;                                //A value for the NTCLE100E3103HT1 in the Steinhart-Hart equation (T = 1/(A + Bln(resistance) + Dln^3(resistance))
-  float b_const = 0.000256985;                                //B value
-  float d_const = 0.00000006383091;                           //D value.
-  float temperature = 1/(a_const+ b_const*log(resistance/10000) + d_const*log(resistance/10000)*log(resistance/10000)*log(resistance/10000)); //Steinhart-Hart to find temperature then return it.  
-  Serial.write("temp = %d", temperature);
+  // Start a temperature measurement:
+  // If request is successful, the number of ms to wait is returned.
+  // If request is unsuccessful, 0 is returned.
+
+  status = pressure.startTemperature();
+  if (status != 0)
+  {
+    // Wait for the measurement to complete:
+
+    delay(status);
+
+    // Retrieve the completed temperature measurement:
+    // Note that the measurement is stored in the variable T.
+    // Use '&T' to provide the address of T to the function.
+    // Function returns 1 if successful, 0 if failure.
+
+    status = pressure.getTemperature(T);
+    if (status != 0)
+    {
+      // Start a pressure measurement:
+      // The parameter is the oversampling setting, from 0 to 3 (highest res, longest wait).
+      // If request is successful, the number of ms to wait is returned.
+      // If request is unsuccessful, 0 is returned.
+
+      status = pressure.startPressure(3);
+      if (status != 0)
+      {
+        // Wait for the measurement to complete:
+        delay(status);
+
+        // Retrieve the completed pressure measurement:
+        // Note that the measurement is stored in the variable P.
+        // Use '&P' to provide the address of P.
+        // Note also that the function requires the previous temperature measurement (T).
+        // (If temperature is stable, you can do one temperature measurement for a number of pressure measurements.)
+        // Function returns 1 if successful, 0 if failure.
+
+        status = pressure.getPressure(P,T);
+        if (status != 0)
+        {
+          file.print(T);
+          file.print("          ");
+          return(P);
+        }
+        else file.println("error retrieving pressure measurement\n");
+      }
+      else file.println("error starting pressure measurement\n");
+    }
+    else file.println("error retrieving temperature measurement\n");
+  }
+  else file.println("error starting temperature measurement\n");
 }
 
 /*
- * servoTest will send a write to the servo to test it, upon which a physical switch will activate a pin if it is hit.
- * The function will monitor this pin, and record to the SD card whether it is hit within the duration given.
- */
-void servoTest(int duration){
+   servoTest will send a write to the servo to test it, upon which a physical switch will activate a pin if it is hit.
+   The function will monitor this pin, and record to the SD card whether it is hit within the duration given.
+*/
+void servoTest(int duration) {
   int milli = 0;
   boolean switch_hit = false;
-  
+
   servo.write(SERVO_STOP);
   servo.write(180);                                         // Start the servo
-  
-  while(!switch_hit && milli < duration){                   // Until the limit switch for the servo is hit, or we have waited for duration in milliseconds.
+
+  while (!switch_hit && milli < duration) {                 // Until the limit switch for the servo is hit, or we have waited for duration in milliseconds.
     switch_hit = digitalRead(SERVO_SWITCH);                 // Read in the digital value of the SERVO_SWITCH pins
     delay(1);
     milli++;                                                    // Increment i every 1 millisecond
   }
   servo.write(SERVO_STOP);
 
-  if(switch_hit){                                           // If the switch has been hit
-    Serial.write("Motor switch hit, took %d milliseconds.\n", milli);
+  if (switch_hit) {                                         // If the switch has been hit
+    file.print("TRUE");
   }
-  else{
-    Serial.write("!!!MOTOR SWITCH NOT HIT!!!!\n");
+  else {
+    file.print("FALSE");
   }
 }
 
 /*
- * solenoidTest will send a write to the servo to test it, upon which a physical switch will activate a pin if it is hit.
- * The function will monitor this pin, and record to the SD card whether it is hit within the duration given.
- */
-void solenoidTest(int duration){
+   solenoidTest will send a write to the servo to test it, upon which a physical switch will activate a pin if it is hit.
+   The function will monitor this pin, and record to the SD card whether it is hit within the duration given.
+*/
+void solenoidTest(int duration) {
   int milli = 0;
   boolean switch_hit = false;
-  
+
   digitalWrite(SOLENOID_PIN, HIGH);                        // Start the solenoid
-  
-  while(!switch_hit && milli < duration){                  // Until the limit switch for the solenoid is hit, or we have waited for duration in milliseconds.
+
+  while (!switch_hit && milli < duration) {                // Until the limit switch for the solenoid is hit, or we have waited for duration in milliseconds.
     switch_hit = digitalRead(SOLENOID_SWITCH);             // Read in the digital value of the SERVO_SWITCH pins
     delay(1);
     milli++;                                                   // Increment i every 1 millisecond
   }
   digitalWrite(SOLENOID_PIN, LOW);
 
-  if(switch_hit){                                          // If the switch has been hit
-    Serial.write("Solenoid switch hit, took %d milliseconds.\n", milli);
+  if (switch_hit) {                                        // If the switch has been hit
+    file.print("TRUE");
   }
-  else{
-    Serial.write("!!!SOLENOID SWITCH NOT HIT!!!!\n");
+  else {
+    file.print("FALSE");
   }
 }
 
