ccc3edac4f3daa6d744a1a8d6bb35a219cf8747a
Sean Widmier <seanwidmier@gmail.com>
2018-09-08 19:28:18 -0500

    Set up flight code

--- RAPTOR/RAPTOR.ino
+++ RAPTOR/RAPTOR.ino
@@ -2,82 +2,144 @@
 #include <Wire.h>
 #include <Servo.h>
 #include <SD.h>
+#include <Adafruit_BNO055.h>
+#include <Adafruit_GPS.h>
+#include <SoftwareSerial.h>
 #include <elapsedMillis.h>
+//#include <Pilot.h>
+
+#define CUTDOWN_ALT 1000 // altitude to cutdown at
 
 #define SERVO_STOP 90
 
-#define SERVO1_PIN 6
-#define SERVO2_PIN 5
-#define SERVO1_SWITCH 4
-#define SERVO2_SWITCH 1
+#define BZZ_DTA 13    // Buzzer
+#define LEDS_DTA 12   // External flight LEDs;
 
-#define SOLENOID1_PIN 9
-#define SOLENOID1_SWITCH 2
+#define SRVOL_DTA 6   // Left servo
+#define SRVOR_DTA 5   // Right servo
 
-#define SOLENOID2_PIN 8
-#define SOLENOID2_SWITCH 3
+#define SOLP_DTA 9    // Parafoil solenoid
+#define SOLC_DTA 8    // Cutdown solenoid
 
+#define SWP_PIN A0    // Parafoil solenoid switch
+#define SWC_PIN A1    // Cutdown solenoid switch
 
-Servo servo1;
-Servo servo2;
+#define LEDP_DTA A2   // Parafoil solenoid indicator light
+#define LEDC_DTA A3   // Cutdown solenoid indicator light
+
+#define SD_GRN 4      // OpenLog Reset pin
+
+Servo servoL;
+Servo servoR;
+
 SFE_BMP180 pressure; //SDA -> A4, SCL -> A5 https://learn.adafruit.com/bmp085/wiring-the-bmp085
-File file;           //CLK -> 13, DO -> 12, DI -> 11, CS -> 10 https://learn.adafruit.com/adafruit-micro-sd-breakout-board-card-tutorial/arduino-wiring
+
 elapsedMillis timeElapsed;
 
+SoftwareSerial mySerial(3, 2); // GPS serial comm pins
+volatile Adafruit_GPS GPS(&mySerial);
+
+boolean flying = false;
+
 float baseline;
+
+double getPressure(void); // provide prototypes for the ISR
+double correctAlt(double bmpressure, double gpspressure);
 
 void setup() {
   timeElapsed = 0;
 
-  servo1.attach(SERVO1_PIN);
-  servo2.attach(SERVO2_PIN);
+  /* Buzzer and LEDs */
+  pinMode(BZZ_DTA, OUTPUT);     // Set buzzer to output
+  pinMode(LEDS_DTA, OUTPUT);    // Set LEDs to output
   
-  pinMode(SERVO1_SWITCH, OUTPUT);
-  pinMode(SERVO2_SWITCH, OUTPUT);
+  /* Servos */ 
+  servoR.attach(SRVOR_DTA);     // Attach right servo
+  servoL.attach(SRVOL_DTA);     // Attach left servo
+
+  /* Solenoids */
+  pinMode(SOLP_DTA, OUTPUT);    // Set Parafoil solenoid to output
+  pinMode(SOLC_DTA, OUTPUT);    // Set Cutdown solenoid to output
+
+  pinMode(SWP_PIN, INPUT);      // Set Parafoil switch to input
+  pinMode(SWC_PIN, INPUT);      // Set Cutdown switch to input
+
+  pinMode(LEDP_DTA, OUTPUT);    // Set Parafoil LED to output
+  pinMode(LEDC_DTA, OUTPUT);    // Set Cutdown LED to output
+
+  digitalWrite(SOLP_DTA, HIGH); // Engage Parafoil solenoid
+  digitalWrite(SOLC_DTA, HIGH); // Engage Cutdown solenoid
   
-  pinMode(SOLENOID1_PIN, OUTPUT);
-  pinMode(SOLENOID2_PIN, OUTPUT);
+  /* BMP180 */
+  pressure.begin();             // Begin pressure measurements
+  baseline = getPressure();     // Grab a baseline pressure
 
-  pinMode(SOLENOID1_SWITCH, OUTPUT);
-  pinMode(SOLENOID2_SWITCH, OUTPUT);
+  /* IMU */
+  
 
-  servo1.write(90);
-  servo2.write(90);
-  /* Pressure */
-  pressure.begin();
-  baseline = getPressure();
+  /* GPS */
+  GPS.begin(9600);
+  
+  GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCGGA);   // RMC (recommended minimum), GGA (fix data) + altitude
+  GPS.sendCommand(PMTK_SET_NMEA_UPDATE_1HZ);      // 1 Hz update rate
+  GPS.sendCommand(PGCMD_ANTENNA);                 // Request updates on antenna status
 
-  digitalWrite(SOLENOID2_PIN, HIGH);
-  /* SD Card */
-  pinMode(10, OUTPUT);
-  SD.begin(10);
-  file.close();
+  interrupt_init();
+
+  delay(1000);
+
+  /* Pilot */
+  //pilot.wake();
+  
+  /* SD */
+  pinMode(SD_GRN, OUTPUT);
+  Serial.begin(9600);
+  
+  // Reset OpenLog
+  digitalWrite(SD_GRN, LOW);
+  delay(100);
+  digitalWrite(SD_GRN, HIGH);
+
+  delay(10);
+  Serial.print(F("TIME, TEMPERATURE, PRESSURE, ALTITUDE, \n")); // data header
+  
 }
 
 void loop() {
-  file = SD.open("data.csv", FILE_WRITE);  //FORMAT: time(sec), temperature(C), pressure(Pascal?), altitude(m), servo 1(bool), servo 2(bool), solenoid 1(bool), solenoid 2(bool) 
   
-  file.print(timeElapsed/1000);
-  file.print(",");
+    if(correctAlt(pressure.altitude(getPressure(),baseline), GPS.altitude) > CUTDOWN_ALT && !flying){ // we have liftoff, or liftdown I guess
+      // may want to do some falling checks here
+      // also may want to deploy our parafoil
+      // and then check the parafoil
+      
+      //pilot.wake();
+      flying = true;
+    }
     
-  double P = getPressure();
-  file.print(P);
-  file.print(",");
-  double a = pressure.altitude(P, baseline);
-  file.print(a);
-  file.print(",");
-
-  if(a > 150){
-    servoTest(1000);
-    
-    solenoidTest(500);
-  }
-  file.print("\n");
-  file.close();
-  delay(5000);
+    // collect IMU data
+    // write everything to SD card
 }
 
-double getPressure(){
+/* Timer0 used for millis(), interrupt in the middle */
+void interrupt_init(void){
+  OCR0A = 0xAF;
+  TIMSK0 |= _BV(OCIE0A);
+}
+
+/* Interrupt on millisecond */
+SIGNAL(TIMER0_COMPA_vect) {
+  GPS.read(); // Check to see if we have new data
+  
+  if(GPS.newNMEAreceived()){
+    if (GPS.parse(GPS.lastNMEA())){   // this also sets the newNMEAreceived() flag to false
+
+      if(flying)
+        ;//pilot.fly(correctAlt(pressure.altitude(getPressure(),baseline), GPS.altitude), GPS.angle); // the pilot needs altitude and angle to do his calculations
+    }
+  }
+}
+
+double getPressure(void){
   char status;
   double T,P,p0,a,b;
 
@@ -123,90 +185,20 @@ double getPressure(){
         status = pressure.getPressure(P,T);
         if (status != 0)
         {
-          file.print(T);
-          file.print(",");
+//          Serial.print(T);
+//          Serial.print(",");
           return(P);
         }
-        else file.println("error retrieving pressure measurement\n");
+//        else Serial.println("error retrieving pressure measurement\n");
       }
-      else file.println("error starting pressure measurement\n");
+//      else Serial.println("error starting pressure measurement\n");
     }
-    else file.println("error retrieving temperature measurement\n");
+//    else Serial.println("error retrieving temperature measurement\n");
   }
-  else file.println("error starting temperature measurement\n");
+//  else Serial.println("error starting temperature measurement\n");
 }
 
-/*
-   servoTest will send a write to the servo to test it, upon which a physical switch will activate a pin if it is hit.
-   The function will monitor this pin, and record to the SD card whether it is hit within the duration given.
-*/
-void servoTest(int duration) {
-  int milli = 0;
-  boolean switch1_hit = false;
-  boolean switch2_hit = false;
-
-  servo1.write(SERVO_STOP);
-  servo2.write(SERVO_STOP);
-
-  delay(10);
-
-  servo1.write(180);
-  servo2.write(180);
-  
-  while (milli < duration) {                 // Until the limit switch for the servo is hit, or we have waited for duration in milliseconds.
-    if(!switch1_hit)
-      switch1_hit = digitalRead(SERVO1_SWITCH);                 // Read in the digital value of the SERVO_SWITCH pins
-    if(!switch2_hit)
-      switch2_hit = digitalRead(SERVO2_SWITCH);
-    delay(1);
-    milli++;                                                    // Increment i every 1 millisecond
-  }
-
-  delay(100);
-  servo1.write(SERVO_STOP);
-  servo2.write(SERVO_STOP);
-
-  if (switch1_hit) {                                         // If the switch has been hit
-    file.print("TRUE,");
-  }
-  else {
-    file.print("FALSE,");
-  }
-
-  if (switch2_hit) {                                         // If the switch has been hit
-    file.print("TRUE,");
-  }
-  else {
-    file.print("FALSE,");
-  }
-}
-
-/*
-   solenoidTest will send a write to the servo to test it, upon which a physical switch will activate a pin if it is hit.
-   The function will monitor this pin, and record to the SD card whether it is hit within the duration given.
-*/
-void solenoidTest(int duration) {
-  int milli = 0;
-  boolean switch_hit = false;
-
-  digitalWrite(SOLENOID1_PIN, HIGH);                        // Start the solenoid
-
-  while (!switch_hit && milli < duration) {                // Until the limit switch for the solenoid is hit, or we have waited for duration in milliseconds.
-    switch_hit = digitalRead(SOLENOID1_SWITCH);             // Read in the digital value of the SERVO_SWITCH pins
-    delay(1);
-    milli++;                                                   // Increment i every 1 millisecond
-  }
-  digitalWrite(SOLENOID1_PIN, LOW);
-
-  if (switch_hit) {                                        // If the switch has been hit
-    file.print("TRUE,");
-  }
-  else {
-    file.print("FALSE,");
-  }
-  if(digitalRead(SOLENOID2_SWITCH))
-    file.print("TRUE");
-  else
-    file.print("FALSE");
+double correctAlt(double bmpressure, double gpspressure){
+  // to write
 }
 
