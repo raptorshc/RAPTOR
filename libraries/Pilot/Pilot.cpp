dcd415e997deac0007c03f321f7ff27e12d96867
Sean Widmier <seanwidmier@gmail.com>
2018-09-09 20:16:35 -0500

    set up Pilot library

---
+++ libraries/Pilot/Pilot.cpp
@@ -0,0 +1,39 @@
+/*
+
+*/
+#include "Pilot.h"
+
+/* PUBLIC METHODS */
+
+/*
+ *  Constructor for Pilot 
+ */
+Pilot::Pilot(){}
+
+/*
+ *  Constructor for Pilot 
+ */
+void Pilot::wake(Coordinate &launch){
+	
+}
+
+/*
+ *  Constructor for Pilot 
+ */
+void Pilot::fly(altitude, angle){
+	
+}
+
+/*
+ *  Constructor for Pilot 
+ */
+void Pilot::rightTurn(){
+	
+}
+
+/*
+ *  Constructor for Pilot 
+ */
+void Pilot::shouldTurn(){
+	
+}
---
+++ libraries/Pilot/Pilot.h
@@ -0,0 +1,29 @@
+/*
+  Pilot.h - 
+	DESCRIPTION NEEDED.
+	Part of the RAPTOR project, authors: Sean Widmier, Colin Oberthur
+*/
+#ifndef PILOT_h
+#define PILOT_h
+
+#include <Pathfinder.h>
+
+class Pilot{
+public:
+    Pilot();
+    
+	void wake(Coordinate &launch);
+	void fly(altitude, angle);
+    
+private:
+	Coordinate &launch;
+	float current_heading, desired_heading;
+	int is_turning;
+	
+	void rightTurn();
+	void leftTurn();
+	void shouldTurn();
+    
+};
+
+#endif
