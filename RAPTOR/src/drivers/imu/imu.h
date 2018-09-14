3e717ffb7879332efbdbd8bc9f793f6c7375fa9b
Sean Widmier <seanwidmier@gmail.com>
2018-09-14 16:23:35 -0500

    add comments, fix #include in pilot

--- RAPTOR/src/drivers/imu/imu.h
+++ RAPTOR/src/drivers/imu/imu.h
@@ -1,7 +1,8 @@
 /*
-  ContinuousServo.h - 
-  Library written for use with continuous rotation servos.
-  Part of the RAPTOR project, authors: Sean Widmier, Colin Oberthur
+ * imu.h - 
+ * Library written for use with Adafruit BNO055 sensor.
+ * Utilizes Adafruit BNO055 library.
+ * Part of the RAPTOR project, authors: Sean Widmier, Colin Oberthur
 */
 #ifndef IMU_H_
 #define IMU_H_
