b7f206180c37a31166734e7151a4ad732ec53ddb
Sean Widmier <seanwidmier@gmail.com>
2018-09-23 00:54:04 -0500

    formatting, resolves #12

--- RAPTOR/src/drivers/imu/imu.h
+++ RAPTOR/src/drivers/imu/imu.h
@@ -10,13 +10,14 @@
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BNO055.h>
 
-class BNO : public Adafruit_BNO055{
-    public:
-        BNO() : Adafruit_BNO055(55){}
-        bool init(void);
-        void update(void);
+class BNO : public Adafruit_BNO055
+{
+  public:
+    BNO() : Adafruit_BNO055(55) {}
+    bool init(void);
+    void update(void);
 
-        sensors_event_t data;
+    sensors_event_t data;
 };
 
 #endif
