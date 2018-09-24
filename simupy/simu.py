3aad792f048020682ae62447306c0b9e449a8010
Sean Widmier <seanwidmier@gmail.com>
2018-09-24 18:56:03 -0500

    test input and printing

--- simupy/simu.py
+++ simupy/simu.py
@@ -1,5 +1,21 @@
 import serial
 
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+
+# class DataPlot:
+#     def __init__(self, port, maxlen):
+#         # open serial port
+#         self.ser = serial.Serial(port, 9600)
+#     def update(self):
+#         nop
+
 ser = serial.Serial('/dev/ttyASCM0', 9600)
+
+time = []
+temperature = []
+pressure = []
+
 while True:
-    print(ser.readline())
+    raw = ser.readline().split(',')
+    print(f'Time: {raw[0]} Temperature: {raw[1]} Pressure: {raw[2]}')
