03ffc2750959389508f681f7ba2f5512dedc1153
Sean Widmier <seanwidmier@gmail.com>
2018-09-26 12:55:36 -0500

    test with file, edit for arduino use

--- simupy/simufile.py
+++ simupy/simufile.py
@@ -15,9 +15,9 @@ import matplotlib.animation as animation
 # plot class
 class plotter:
   # constr
-  def __init__(self, strPort, maxLen):
-      # open serial port
-      self.ser = serial.Serial(strPort, 9600)
+  def __init__(self, file, maxLen):
+      # open file
+      self.inf = open(file,"r")
 
       self.ax = deque([0.0]*maxLen)
       self.ay = deque([0.0]*maxLen)
@@ -40,13 +40,15 @@ class plotter:
   # update plot
   def update(self, frameNum, a0, a1):
       try:
-          line = self.ser.readline()
-          data = [float(val) for val in line.split(',')]
-          # print data
-          if(len(data) == 2):
-              self.add(data)
-              a0.set_data(range(self.maxLen), self.ax)
-              a1.set_data(range(self.maxLen), self.ay)
+          line = self.inf.readline().strip()
+          if line:
+            print(f'line:{line}')
+            data = [float(val) for val in line.split(',')]
+            # print data
+            if(len(data) == 2):
+                self.add(data)
+                a0.set_data(range(self.maxLen), self.ax)
+                a1.set_data(range(self.maxLen), self.ay)
       except KeyboardInterrupt:
           print('exiting')
       
@@ -54,27 +56,15 @@ class plotter:
 
   # clean up
   def close(self):
-      # close serial
-      self.ser.flush()
-      self.ser.close()    
+      # close file
+      self.inf.flush()
+      self.inf.close()    
 
 # main() function
 def main():
-  # create parser
-  parser = argparse.ArgumentParser(description="LDR serial")
-  # add expected arguments
-  parser.add_argument('--port', dest='port', required=True)
-
-  # parse args
-  args = parser.parse_args()
-  
-  #strPort = '/dev/tty.usbserial-A7006Yqh'
-  strPort = args.port
-
-  print('reading from serial port %s...' % strPort)
 
   # plot parameters
-  plot = plotter(strPort, 100)
+  pl = plotter("test.txt", 100)
 
   print('plotting data...')
 
@@ -83,7 +73,7 @@ def main():
   ax = plt.axes(xlim=(0, 100), ylim=(0, 1023))
   a0, = ax.plot([], [])
   a1, = ax.plot([], [])
-  anim = animation.FuncAnimation(fig, plot.update, 
+  anim = animation.FuncAnimation(fig, pl.update, 
                                  fargs=(a0, a1), 
                                  interval=50)
 
@@ -91,7 +81,7 @@ def main():
   plt.show()
   
   # clean up
-  plot.close()
+  pl.close()
 
   print('exiting.')
   
