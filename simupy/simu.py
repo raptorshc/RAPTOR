cd15452fa6a24e96fcfa38c606709dec8526b02b
Sean Widmier <seanwidmier@gmail.com>
2018-09-26 12:20:31 -0500

    use legacy code

--- simupy/simu.py
+++ simupy/simu.py
@@ -1,28 +1,101 @@
-import serial
+"""
+simu.py
+Displays data output from Arduino using matplotlib.
+Original code: https://gist.github.com/electronut/d5e5f68c610821e311b0
+"""
+import sys, serial, argparse
+import numpy as np
+from time import sleep
+from collections import deque
 
-import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt 
 import matplotlib.animation as animation
 
-# class DataPlot:
-#     def __init__(self, port, maxlen):
-#         # open serial port
-#         self.ser = serial.Serial(port, 9600)
-#     def update(self):
-#         nop
+    
+# plot class
+class AnalogPlot:
+  # constr
+  def __init__(self, strPort, maxLen):
+      # open serial port
+      self.ser = serial.Serial(strPort, 9600)
 
-ser = serial.Serial('/dev/ttyACM0', 9600)
+      self.ax = deque([0.0]*maxLen)
+      self.ay = deque([0.0]*maxLen)
+      self.maxLen = maxLen
 
-time = []
-temperature = []
-pressure = []
+  # add to buffer
+  def addToBuf(self, buf, val):
+      if len(buf) < self.maxLen:
+          buf.append(val)
+      else:
+          buf.pop()
+          buf.appendleft(val)
 
-# fig = plt.figure()
-# ax1 = fig.add_subplot(1,1,1)
+  # add data
+  def add(self, data):
+      assert(len(data) == 2)
+      self.addToBuf(self.ax, data[0])
+      self.addToBuf(self.ay, data[1])
 
-# def animate(i):
-#     raw = ser.readline.split(',')
+  # update plot
+  def update(self, frameNum, a0, a1):
+      try:
+          line = self.ser.readline()
+          data = [float(val) for val in line.split(',')]
+          # print data
+          if(len(data) == 2):
+              self.add(data)
+              a0.set_data(range(self.maxLen), self.ax)
+              a1.set_data(range(self.maxLen), self.ay)
+      except KeyboardInterrupt:
+          print('exiting')
+      
+      return a0, 
 
-while True:
-    raw = ser.readline().split(',')
-    print(raw)
-    # print(f'Time: {raw[0]} Temperature: {raw[1]} Pressure: {raw[2]}')
+  # clean up
+  def close(self):
+      # close serial
+      self.ser.flush()
+      self.ser.close()    
+
+# main() function
+def main():
+  # create parser
+  parser = argparse.ArgumentParser(description="LDR serial")
+  # add expected arguments
+  parser.add_argument('--port', dest='port', required=True)
+
+  # parse args
+  args = parser.parse_args()
+  
+  #strPort = '/dev/tty.usbserial-A7006Yqh'
+  strPort = args.port
+
+  print('reading from serial port %s...' % strPort)
+
+  # plot parameters
+  analogPlot = AnalogPlot(strPort, 100)
+
+  print('plotting data...')
+
+  # set up animation
+  fig = plt.figure()
+  ax = plt.axes(xlim=(0, 100), ylim=(0, 1023))
+  a0, = ax.plot([], [])
+  a1, = ax.plot([], [])
+  anim = animation.FuncAnimation(fig, analogPlot.update, 
+                                 fargs=(a0, a1), 
+                                 interval=50)
+
+  # show plot
+  plt.show()
+  
+  # clean up
+  analogPlot.close()
+
+  print('exiting.')
+  
+
+# call main
+if __name__ == '__main__':
+  main()
