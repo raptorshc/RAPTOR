d1a345210301bb0342a3781defc5da201b247bf8
Sean Widmier <seanwidmier@gmail.com>
2018-09-28 16:27:09 -0500

    change to live-plot, add three axis funtionality to plotter

--- simupy/live-plot.py
+++ simupy/live-plot.py
@@ -1,5 +1,5 @@
 """
-simu.py
+live-plot.py
 Displays data output from a serial port using matplotlib.
 Original code: https://gist.github.com/electronut/d5e5f68c610821e311b0
 """
@@ -18,7 +18,7 @@ import matplotlib.animation as animation
 # plot class
 class plotter:
     # constr
-    def __init__(self, strPort, maxLen, axis):
+    def __init__(self, strPort, maxLen, axis1, axis2=False):
         # open serial port
         self.ser = serial.Serial(strPort,baudrate=9600,
                      bytesize=serial.EIGHTBITS,
@@ -35,9 +35,13 @@ class plotter:
 
         self.ax = deque([0.0]*maxLen)
         self.ay = deque([0.0]*maxLen)
+        self.ay2 = deque([0.0]*maxLen)
+
         self.maxLen = maxLen
 
-        self.axis = axis
+        self.axis1 = axis1
+        self.axis2 = axis2 # ay2
+
         self.xlim = 200
         self.ylim = 10000
 
@@ -54,6 +58,9 @@ class plotter:
         self.addToBuf(self.ax, data[0])
         self.addToBuf(self.ay, data[1])
 
+        if self.axis2:
+            self.addToBuf(self.ay2, data[2])
+
     # update plot
     def update(self, frameNum, a0, a1):
         try:
@@ -61,17 +68,25 @@ class plotter:
             if line:
                 data = [float(val) for val in line.split(',') if val]
                 print(data)
-                if(len(data) > 10):
+                if(len(data) > 3):
                     self.add(data)
                     a0.set_data(self.ax, self.ay)
+                    if self.axis2:
+                        a1.set_data(self.ay2)
+                        
 
                     # update the limits
                     if(data[0] > self.xlim):
-                        self.axis.set_xlim([0,data[0]])
+                        self.axis1.set_xlim([0,data[0]])
                         self.xlim = data[0]
                     if(data[1] > self.ylim):
-                        self.axis.set_ylim([0,data[1]])
                         self.ylim = data[1]
+                        self.axis1.set_ylim([0,self.ylim)
+                    if self.axis2:
+                        if(data[2] > self.ylim):
+                            self.ylim = data[2]
+                        self.axis1.set_ylim([0,self.ylim])    
+                        self.axis2.set_ylim([0,self.ylim]) # some logic issues with this might need to fix
         except KeyboardInterrupt:
             print('exiting')
         except ValueError:
@@ -90,27 +105,44 @@ class plotter:
 
 def main():
     # create parser
-    parser = argparse.ArgumentParser(description="LDR serial")
-    # add expected arguments
     parser.add_argument('--port', dest='port', required=True)
-    parser.add_argument('--data', dest='data', required=False)
+    parser.add_argument('--data', dest='datalist', nargs = '+', required=False)
 
     # parse args
     args = parser.parse_args()
 
     # strPort = '/dev/ttyACM0'
     strPort = args.port
-    data = { i : 0 for i in args.data.split() }
+    data = { i : 0 for i in args.datalist }
 
     print('reading from serial port %s...' % strPort)
     print('data: ')
     for key in data.keys():
         print(key + ', ')
 
-    # plot parameters
-    fig = plt.figure()
-    ax = plt.axes(xlim=(0,200), ylim=(0,10000))
-    plot = plotter(strPort, 100, ax)
+    # build plots
+    # if len(data) > 3
+    #     fig1, ax = plt.subplots()
+    #     ax.plot()
+    #     ax.set_xlabel(data.keys()[1])
+    #     ax.set_ylabel(data.keys()[2])
+    if len(data) > 2:
+        fig, ax1 = plt.subplots()
+        ax1.plot()
+        ax1.set_xlabel(data.keys()[1])
+        ax1.set_ylabel(data.keys()[2])
+
+        ax2 = ax1.twinx()
+        ax2.plot(color='r')
+        ax2.set_ylabel(data.keys[3])
+
+        plot = plotter(strPort, 100, axis1=ax1, axis2=ax2)
+    else:
+        fig = plt.figure()
+        ax = plt.axes(xlim=(0,1000), ylim=(0,1000))
+        ax.set_xlabel(data.keys()[1])
+        ax.set_ylabel(data.keys()[2])
+        plot = plotter(strPort, 100, ax)
 
     print('plotting data...')
 
