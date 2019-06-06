6faca9128616a6cf9609bf66b5e853e862fde1a7
Sean Widmier <seanwidmier@gmail.com>
2019-06-05 20:22:49 -0500

    removed old folders

--- software/libraries/Skynet/Connections.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-struct Connections 
-{
-	double m_dWeight;
-	double m_dDeltaWeight;
-};
--- software/libraries/Skynet/Layer.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <vector>
-//#include "Neuron.h"
-
-using namespace std;
-
-class Neuron;
-typedef vector<Neuron> Layer;
